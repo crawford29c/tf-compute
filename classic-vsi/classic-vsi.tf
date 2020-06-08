fdcdcd714d0aac1f7c0032e3e9e5fa925b546ffe
Charlie Crawford <ccraw@us.ibm.com>
2020-06-08 12:28:18 -0500

    Update script

--- classic-vsi/classic-vsi.tf
+++ classic-vsi/classic-vsi.tf
@@ -1,3 +1,11 @@
+# provider configuration parameters/variables
+provider "ibm" {
+  ibmcloud_api_key   = var.ibmcloud_api_key
+  region             = "us-south"
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
+}
+
 resource "ibm_compute_vm_instance" "vm1" {
 hostname             = "vm1"
 domain               = "example.com"
