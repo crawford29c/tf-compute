24db821061afd77a4bea3bf84de5cf3e80e576e0
Charlie Crawford <ccraw@us.ibm.com>
2020-06-08 12:13:28 -0500

    Initial Check In

---
+++ classic-vsi/variables.tf
@@ -0,0 +1,14 @@
+variable "ibmcloud_api_key" {
+  # defined in tfvars
+}
+variable "ssh_key" {
+  # defined in tfvars
+}
+
+variable "iaas_classic_username" {
+ # defined in tfvars
+}
+
+variable "iaas_classic_api_key" {
+  # defined in tfvars
+}
