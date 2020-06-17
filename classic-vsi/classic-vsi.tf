91523d7cc80bf74441db3adc91e751428a8180d6
Charlie Crawford <ccraw@us.ibm.com>
2020-06-17 16:38:43 -0500

    Add subnet stuff

--- classic-vsi/classic-vsi.tf
+++ classic-vsi/classic-vsi.tf
@@ -1,31 +1,27 @@
 # provider configuration parameters/variables
 provider "ibm" {
-  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
+  ibmcloud_api_key   = var.ibmcloud_api_key
   generation = 2
-  region             = "${var.region}"
-  iaas_classic_username = "${var.iaas_classic_username}" 
-  iaas_classic_api_key  = "${var.iaas_classic_api_key}" 
+  region             = var.region
+  iaas_classic_username = var.iaas_classic_username
+  iaas_classic_api_key  = var.iaas_classic_api_key
 }
 
-# make OS image details visible by using name in variables.tf
-data ibm_is_image "image_id" {
-  name = "${var.image}"
+data "ibm_is_vpc" "myvpc" {
+    name = var.vpcname
 }
-
-resource "ibm_is_instance" "new_vsi" {
-name  = "{$var.hostname}"
-vpc     = "${var.vpcname}"
-keys    = ["${data.ibm_is_ssh_key.ssh_key_id.id}"]
-zone    = "${var.region}-1"
-image   = "${data.ibm_is_image.image_id.id}"
-profile = "${var.profile}"
+data "ibm_resource_group" "myrg" {
+    name = "default"
 }
-
-resource "ibm_is_floating_ip" "fip1" {
-  name   = "${local.BASENAME}-fip1"
-  target = ibm_is_instance.new_vsi.primary_network_interface[0].id
-}
-
-output "sshcommand" {
-  value = "ssh root@ibm_is_floating_ip.fip1.address"
+resource "ibm_is_instance" "instance" {
+  name    = var.hostname
+  image   = var.image
+  profile = var.profile
+  primary_network_interface {
+    subnet          = data.ibm_is_vpc.myvpc.subnets[0].id
+  }
+  resource_group = data.ibm_resource_group.myrg.id
+  vpc       = data.ibm_is_vpc.myvpc.id
+  zone    = "${var.region}-1"
+  keys      = [var.ssh_key]
 }
