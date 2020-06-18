56bd844b094e3e249b5fd4e681948e4ed916c4d5
Charlie Crawford <ccraw@us.ibm.com>
2020-06-18 13:53:23 -0500

    fix variable name

--- classic-vsi/classic-vsi.tf
+++ classic-vsi/classic-vsi.tf
@@ -2,21 +2,21 @@
 provider "ibm" {
   ibmcloud_api_key   = "${var.ibmcloud_api_key}"
   generation = 2
-  region             = var.region
-  iaas_classic_username = var.iaas_classic_username
-  iaas_classic_api_key  = var.iaas_classic_api_key
+  region             = "${var.region}"
+  iaas_classic_username = "${var.iaas_classic_username}"
+  iaas_classic_api_key  = "${var.iaas_classic_api_key}"
 }
 
 data "ibm_is_vpc" "myvpc" {
-    name = var.vpcname
+    name = "${var.vpcname}"
 }
 data "ibm_resource_group" "myrg" {
     name = "default"
 }
 resource "ibm_is_instance" "instance" {
-  name    = var.hostname
-  image   = var.image
-  profile = var.profile
+  name    = "${var.hostname}"
+  image   = "${var.image}"
+  profile = "${var.profile}"
   primary_network_interface {
     subnet          = data.ibm_is_vpc.myvpc.subnets[0].id
   }
