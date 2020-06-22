4e259dc69083cf09882530810be2d6d639c98175
Charlie Crawford <ccraw@us.ibm.com>
2020-06-22 13:41:10 -0500

    fix variable name

--- classic-vsi/classic-vsi.tf
+++ classic-vsi/classic-vsi.tf
@@ -14,11 +14,6 @@ data "ibm_resource_group" "myrg" {
     name = "${var.resource_group}"
 }
 
-# make OS image details visible by using name in variables.tf
-data ibm_is_image "image_id" {
-  name = "${var.image}"
-}
-
 # make ssh key details visible by using name in variables.tf
 data ibm_is_ssh_key "ssh_key_id" {
   name = "${var.ssh_key}"
