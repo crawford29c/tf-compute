a6dd7af28ba4ce57d572d545f6a1a4f23ed031ab
Charlie Crawford <ccraw@us.ibm.com>
2020-06-22 10:30:18 -0500

    fix variable name

--- classic-vsi/classic-vsi.tf
+++ classic-vsi/classic-vsi.tf
@@ -29,7 +29,6 @@ resource "ibm_is_image" "vnf_custom_image" {
   depends_on       = ["random_uuid.test"]
   href             = "${var.vnf_cos_image_url}"
   name             = "${var.vnf_vpc_image_name}-${substr(random_uuid.test.result,0,8)}"
-  operating_system = "red-7-amd64"
   resource_group = "${data.ibm_resource_group.myrg.id}"
 
   timeouts {
